5b799a5de7fafc7a2ef65feb8120a4d066698456
PrashantSoni-coder <soniprashant562005@gmail.com>
2025-06-28 10:59:42 +0530

    add two new routs home and show

---
+++ app.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const app = express();
+
+const mongoose = require("mongoose");
+const path = require("path");
+const Listing = require("./models/listing.js");
+
+
+app.set("view engine","ejs");
+app.set("views" , path.join(__dirname,"views"));
+app.use(express.urlencoded({extended:true}));
+
+const mongo_url ="mongodb://127.0.0.1:27017/wanderlust";
+
+main().then(()=>{
+    console.log("DB connected")
+}).catch((err)=>{
+    console.log(err);
+})
+
+async function main(){
+    await mongoose.connect(mongo_url)
+}
+
+
+
+// app.get("/testlisting",(req,res)=>{
+//     let samplelisting= new Listing({
+//         title:" NOt My New villa",
+//         description: "my beach",
+//         price: 1200,
+//         location:"Calangute, Goa",
+//         country:"India",
+//     });
+
+//     samplelisting.save().then(()=>{
+//         console.log("listing Sucessfull!")
+//         res.send ("success");
+//     }).catch((err)=>{
+//         console.log(err);
+//     })
+// })
+
+app.get("/" , (req,res)=>{
+    res.send("root is on");
+})
+//index/home page
+app.get("/listings",async (req,res)=>{
+    let allListings = await Listing.find()
+    res.render("listings/index.ejs",{allListings});
+})
+
+//show rout
+
+app.get("/listings/:id",async (req,res)=>{
+    let {id} = req.params;
+    res.render("listings/show.ejs");
+})
+
+
+app.listen(8080 ,()=>{
+    console.log("app listening at port: 8080")
+})
