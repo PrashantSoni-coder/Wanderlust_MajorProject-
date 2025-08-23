83d9b254b4b56ac002c39c4d6f7bdb5eec420f89
PrashantSoni-coder <soniprashant562005@gmail.com>
2025-08-23 14:56:28 +0530

    fixed nav and tax summary

--- app.js
+++ app.js
@@ -26,8 +26,8 @@ const LocalStrategy = require("passport-local");
 const User = require("./models/user.js");
 
 
-// const mongo_url ="mongodb://127.0.0.1:27017/wanderlust";
-const dbUrl = process.env.ATLASDB_URL;
+const mongo_url ="mongodb://127.0.0.1:27017/wanderlust";
+// const dbUrl = process.env.ATLASDB_URL;
 
 const session = require("express-session");
 const MongoStore = require('connect-mongo');
@@ -41,10 +41,10 @@ main().then(()=>{
 })
 
 async function main(){
-    await mongoose.connect(dbUrl)
+    await mongoose.connect(mongo_url)
 }
 const store = MongoStore.create({
-    mongoUrl:dbUrl,
+    mongoUrl:mongo_url,
     crypto: {
         secret : process.env.SECRET,
     },
@@ -88,6 +88,13 @@ app.use((req,res,next)=>{
 app.use("/listings",listingsRouter);
 
 app.use("/listings/:id/reviews",reviewsRouter);
+
+
+// redirecting to correct path
+app.get("/", (req, res) => {
+  res.redirect("/listings");
+});
+
 
 app.use("/",userRouter);
 
