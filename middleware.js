dd4ccce84a60ac8be83bab77406347e885f60db5
Prashant Soni <prashantsoni@Prashants-MacBook-Air-6.local>
2026-05-30 23:03:29 +0530

    ui changes for mobile view

--- middleware.js
+++ middleware.js
@@ -6,7 +6,7 @@ const {listingSchema,reviewSchema} = require("./schema.js");
 module.exports.isLoggedIn = (req,res,next)=>{
     if(!req.isAuthenticated()){
         req.session.redirectUrl = req.originalUrl;
-        req.flash("error","you must be logged in to create listings!")
+        req.flash("error","you must  be logged in to create listings!")
         return res.redirect("/login");
 
     };
